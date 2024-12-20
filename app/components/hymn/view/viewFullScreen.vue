f395389d946b33874ce0498b91216e680c6396f4
Cybandy <andyboat75@gmail.com>
2024-12-21 00:57:42 +0100

    nm

--- app/components/hymn/view/viewFullScreen.vue
+++ app/components/hymn/view/viewFullScreen.vue
@@ -51,41 +51,42 @@ function outOfStanza(diff: number) {
   else return false
 }
 
+const transitionName = ref<string|'left'|'right'>('')
 function next() {
   if (!outOfStanza(1)) {
+    transitionName.value = 'left'
     curr_stanza.value++
   }
 }
 function prev() {
   if (!outOfStanza(-1)) {
+    transitionName.value = 'right'
     curr_stanza.value--
   }
 }
 
 onKeyStroke('ArrowLeft', (e) => {
   e.preventDefault()
-  if(!is_full_screen.value) return
+  if (!is_full_screen.value) return
   prev()
 })
 
 onKeyStroke('ArrowRight', (e) => {
   e.preventDefault()
   if (!is_full_screen.value) return
-  console.log('before',curr_stanza.value)
   next()
-  console.log('after',curr_stanza.value)
 })
 
 // swipe
 const containerEl = ref(null)
-const { direction:swipeDirection } = useSwipe(containerEl)
+const { direction: swipeDirection } = useSwipe(containerEl)
 
 watch(swipeDirection, () => {
   if (swipeDirection.value == 'left') next()
   if (swipeDirection.value == 'right') prev()
 })
 
-const {} = useScreenOrientation()
+const { } = useScreenOrientation()
 </script>
 
 <template>
@@ -93,28 +94,64 @@ const {} = useScreenOrientation()
     <h1 class="text-xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-semibold w-full text-center py-5 md:py-10">
       {{ hymns[0]?.number }} - {{ hymns[0]?.title }}
     </h1>
-    <div class="grid gap-10 w-full h-full">
-      <template v-for="j of Array.from(Array(num_stanzas).keys())" :key="j">
-        <div v-if="j === curr_stanza">
+    <div class="w-full h-full">
+      <TransitionGroup :name="transitionName" tag="div">
+        <template v-for="j of Array.from(Array(num_stanzas).keys())" :key="j">
+          <div v-if="j === curr_stanza">
 
-          <div class="grid gap-8 xl:gap-10" :class="[hymns.length == 2 && 'md:grid-cols-2 md:gap-3']">
+            <div class="grid gap-8 xl:gap-10" :class="[hymns.length == 2 && 'md:grid-cols-2 md:gap-3']">
 
-            <div v-for="(hymn, i) of hymns" :key="i">
-              <div v-if="hymn.stanzas[j]" class="text-gray-800 dark:text-gray-200 text-pretty" :class="[textSize()]">
-                <p v-if="hymn.stanzas[j].is_refrain" class="text-primary font-semibold leading-tight">
-                  {{ refrainLabel(i) }}
-                </p>
-                <p v-else class="font-bold text-lg text-black dark:text-white">{{ hymn.stanzas[j].number }}</p>
-                <div class="space-y-1 leading-tight ">
-                  <p v-for="(line, k) of hymn.stanzas[j].verse.split('\n')" class="first-letter:uppercase">{{ line }}
+              <div v-for="(hymn, i) of hymns" :key="i">
+                <div v-if="hymn.stanzas[j]" class="text-gray-800 dark:text-gray-200 text-pretty" :class="[textSize()]">
+                  <p v-if="hymn.stanzas[j].is_refrain" class="text-primary font-semibold leading-tight">
+                    {{ refrainLabel(i) }}
                   </p>
+                  <p v-else class="font-bold text-lg text-black dark:text-white">{{ hymn.stanzas[j].number }}</p>
+                  <div class="space-y-1 leading-tight ">
+                    <p v-for="(line, k) of hymn.stanzas[j].verse.split('\n')" class="first-letter:uppercase">{{ line }}
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </template>
+        </template>
+      </TransitionGroup>
+
 
     </div>
   </UContainer>
 </template>
+
+<style scoped>
+.left-enter-active,
+.right-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.left-leave-active,
+.right-leave-active {
+  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.left-enter-from,
+.left-leave-to {
+  transform: translateX(-50px);
+}
+.right-enter-from,
+.right-leave-to {
+  transform: translateX(50px);
+  opacity: 0;
+}
+
+/* we will explain what these classes do next! */
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
